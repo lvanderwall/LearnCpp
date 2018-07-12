1ded5f94bcf4e41a3ab71bdb4bb76f035853d525
Lennert van der Wall <l.wall@tu-bs.de>
2018-07-12 12:23:33 +0200

    finish tutorial 01.12 a3)

---
+++ tutorial_01_12/io.h
@@ -0,0 +1,7 @@
+#ifndef IO_H
+#define IO_H
+
+int readNumber();           // read one integer from console
+void writeAnswer(int x);    // print output line
+
+#endif // IO_H
--- tutorial_01_12/main.cpp
+++ tutorial_01_12/main.cpp
@@ -1,9 +1,5 @@
 // #include <iostream> not necessary: It's not used in this file!
-
-
-// forward declarations for functions in io.cpp
-int readNumber();           // read one integer from console
-void writeAnswer(int x);    // print output line
+#include "io.h"
 
 
 int main()
