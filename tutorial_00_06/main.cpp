ac93733c8a86694797c220ab034247d73085b1b9
Lennert van der Wall <l.wall@tu-bs.de>
2018-07-12 08:26:32 +0200

    finish tutorial 00.06

---
+++ tutorial_00_06/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>     // for std::cout (standard output) and std::endl
+
+int main()              // every program needs to have a function "int main()"
+{
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
