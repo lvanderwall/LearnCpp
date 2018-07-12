8635af7cdd07785bfdbee839b5434f906852ee66
Lennert van der Wall <l.wall@tu-bs.de>
2018-07-12 08:46:57 +0200

    finish tutorial 01.04a a5)

--- tutorial_01_04a/main.cpp
+++ tutorial_01_04a/main.cpp
@@ -1,5 +1,24 @@
+#include <iostream>     // for std::cin, std::cout (standard in-/ output) and std::endl
+
+
 // doubles input value
 int doubleNumber(int x)
 {
     return 2 * x;
+}
+
+
+int main()              // every program needs to have one function "int main()"
+{
+    // define an integer variable
+    int i;
+
+    // reads one int from the console
+    std::cout << "Input an integer: ";
+    std::cin >> i;
+
+    // print results
+    std::cout << "Result: " << doubleNumber(i) << std::endl;
+
+    return 0;
 }
