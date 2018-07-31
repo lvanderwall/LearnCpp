d2f5df3585d90767458ab1b94f1c42e42b6d5b1d
Lennert van der Wall <l.wall@tu-bs.de>
2018-07-31 16:18:26 +0200

    finish tutorial 05.07 a2)

--- tutorial_05_07/main.cpp
+++ tutorial_05_07/main.cpp
@@ -1,4 +1,4 @@
-#define quiz1
+#define quiz2
 #ifdef quiz1
 #include <iostream>
 
@@ -11,3 +11,24 @@ int main()
     return 0;
 }
 #endif // quiz1
+#ifdef quiz2
+#include <iostream>
+
+
+// returns sum(i = 1; i <= 5; i)
+int sumTo(int maxVal)
+{
+    int sum{ 0 };   // use maxVal as for counter => no initialization in for loop
+    for(; maxVal > 0; --maxVal)
+        sum += maxVal;
+
+    return sum;
+}
+
+int main()
+{
+    std::cout << sumTo(5);
+
+    return 0;
+}
+#endif // quiz2
