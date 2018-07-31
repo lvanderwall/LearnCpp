3bdbfc087b29c5573b70c3be8d809ee49d3442e1
Lennert van der Wall <l.wall@tu-bs.de>
2018-07-31 15:49:17 +0200

    finish tutorial 05.05 a4)

--- tutorial_05_05/main.cpp
+++ tutorial_05_05/main.cpp
@@ -1,4 +1,4 @@
-#define quiz3
+#define quiz4
 
 #ifdef quiz2
 #include <iostream>
@@ -37,3 +37,29 @@ int main()
     return 0;
 }
 #endif // quiz3
+#ifdef quiz4
+#include <iostream>
+
+// Loop between 1 and 5
+int main()
+{
+    int outer = 1;
+    while (outer <= 5)
+    {
+        // loop between 1 and outer
+        int inner = 5;
+        while (inner >= 1) {
+            if(inner > outer) std::cout << "  ";
+            else              std::cout << inner << ' ';
+
+            --inner;
+        }
+
+        // print a newline at the end of each row
+        std::cout << "\n";
+        ++outer;
+    }
+
+    return 0;
+}
+#endif // quiz4
