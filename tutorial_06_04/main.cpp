640413b70feb01482689b3a184b51650ae07f58f
Lennert van der Wall <l.wall@tu-bs.de>
2018-08-04 15:07:56 +0200

    finish tutorial 06.04 a2)

--- tutorial_06_04/main.cpp
+++ tutorial_06_04/main.cpp
@@ -1,4 +1,4 @@
-#define quiz1
+#define quiz2
 
 #ifdef quiz1
 /*
@@ -17,3 +17,39 @@
     { 10, 20, 30, 40, 50, 60 }      // after selection sort
 */
 #endif // quiz1
+#ifdef quiz2
+#include <utility> // for std::swap, use <algorithm> instead if older than C++11
+#include <iostream>
+
+int main()
+{
+    const int length = 5;
+    int array[length] = { 30, 50, 20, 10, 40 };
+
+    // Step through each element of the array
+    // (except the last one, which will already be sorted by the time we get there)
+    for (int startIndex = 0; startIndex < length - 1; ++startIndex) {
+        // largestIndex is the index of the largest element we’ve encountered this iteration
+        // Start by assuming the largest element is the first element of this iteration
+        int largestIndex = startIndex;
+
+        // Then look for a larger element in the rest of the array
+        for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex) {
+            // If we've found an element that is larger than our previously found largest
+            if (array[currentIndex] > array[largestIndex])
+                // then keep track of it
+                largestIndex = currentIndex;
+        }
+
+        // largestIndex is now the largest element in the remaining array
+        // swap our start element with our largest element (this sorts it into the correct place)
+        std::swap(array[startIndex], array[largestIndex]);
+    }
+
+    // Now that the whole array is sorted, print our sorted array as proof it works
+    for (int index = 0; index < length; ++index)
+        std::cout << array[index] << ' ';
+
+    return 0;
+}
+#endif // quiz2
