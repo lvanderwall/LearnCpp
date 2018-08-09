d873e8aa0da12a4638d547f101f12291cd89024b
Lennert van der Wall <l.wall@tu-bs.de>
2018-08-09 23:41:53 +0200

    finish tutorial 07.08 a1e)

--- tutorial_07_08/main.cpp
+++ tutorial_07_08/main.cpp
@@ -1,6 +1,7 @@
 #define quiz1
 
 #ifdef quiz1
+#include <iostream>
 #include "calculator.h"
 
 
@@ -10,6 +11,11 @@ int main()
     char op{ getOperator() };
     int y{ getInt() };
 
+    std::cout << "---------------------------------\n";
+//    std::cout << x << ' ' << op << ' ' << y << " =\t\t\t\t"   // use explicit derefencing
+//              << (*getArithmeticFuntion(op))(x, y) << '\n';
+    std::cout << x << ' ' << op << ' ' << y << " =\t\t\t\t"
+              << getArithmeticFuntion(op)(x, y) << '\n';
     return 0;
 }
 #endif // quiz1
