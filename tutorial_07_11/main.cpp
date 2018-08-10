22b19f2d5b83691aca80a331f87969d666c8a133
Lennert van der Wall <l.wall@tu-bs.de>
2018-08-10 09:37:33 +0200

    finish tutorial 07.11 a2)

--- tutorial_07_11/main.cpp
+++ tutorial_07_11/main.cpp
@@ -1,4 +1,4 @@
-#define quiz1
+#define quiz2
 
 #ifdef quiz1
 #include <iostream>
@@ -19,3 +19,22 @@ int main()
     return 0;
 }
 #endif // quiz1
+#ifdef quiz2
+#include <iostream>
+
+
+// calculates digit sum for n in N_0 with integer division and modulus
+// using "dS(n) = dS(n / 10) + (n % 10),   init. cond.: dS(n) = n for n < 10
+int digitSum(int n)
+{
+    return (n > 10) ? digitSum(n / 10) + (n % 10) : n; // termination: 0 <= n < 10, n < 0 invalid
+}
+
+
+int main()
+{
+    std::cout << "digitSum(93427) = " << digitSum(93427) << '\n';
+
+    return 0;
+}
+#endif // quiz2
